a6440d8cba069d4dd8f89de32f23a35beb5ee4a2
Kaciade <valerypolyakov60@gmail.com>
2023-08-07 16:11:30 +0200

    всё исправлено

---
+++ Task_2.java
@@ -0,0 +1,12 @@
+public class Task_2 {
+    public static void main(String[] args) {
+        try {
+            int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // intArray создан, чтобы далее его можно было использовать
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
